e05aac5c977468e7fd7b81e28496ae42349900d0
Ketan Patel <ktnp1204@gmail.com>
2024-05-15 22:43:01 +0200

    Created main scene and navigation to game scene

--- singletons/game_manager.gd
+++ singletons/game_manager.gd
@@ -5,3 +5,15 @@ const GROUP_PLAYER: String = "player"
 
 # Speed of the pipes scrolling
 const SCROLL_SPEED: float = 120.0
+
+# Preload the main and game scenes into the build 
+var main_scene: PackedScene = preload("res://scenes/main/main.tscn")
+var game_scene: PackedScene = preload("res://scenes/game/game.tscn")
+
+# Function to load the game scene
+func load_game_scene() -> void:
+	get_tree().change_scene_to_packed(game_scene)
+
+# Function to load the main scene
+func load_main_scene() -> void:
+	get_tree().change_scene_to_packed(main_scene)
